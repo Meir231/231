ecba17b63ffa13b265a6792e5cf3c63bd1a8533d
Meir231 <145269212+Meir231@users.noreply.github.com>
2023-09-17 19:16:14 +0600

    All file. Just want to show

---
+++ password generator/generator.py
@@ -0,0 +1,31 @@
+from random import randint
+
+c = 1
+
+b = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '$', '^', '&', '*', '(', ')', '-', '_', '=', '+', '`', '{', '}', ':', ';', '"', ',', '|']
+
+while c == 1:
+    a = randint(0, 79)
+    d = randint(0, 79)
+    e = randint(0, 79)
+    f = randint(0, 79)
+    g = randint(0, 79)
+    h = randint(0, 79)
+    i = randint(0, 79)
+    j = randint(0, 79)
+    k = randint(0, 79)
+    l = randint(0, 79)
+    m = randint(0, 79)
+    n = randint(0, 79)
+    o = randint(0, 79)
+    p = randint(0, 79)
+    r = randint(0, 79)
+    s = randint(0, 79)
+    t = randint(0, 79)
+    q = randint(0, 79)
+    u = randint(0, 79)
+    v = randint(0, 79)
+    y = randint(0, 79)
+    x = randint(0, 79)
+    c += 1
+    print(b[a] + b[c] + b[d]+ b[e] + b[f]+ b[g] + b[h]+ b[i] + b[j]+ b[l] + b[m]+ b[n] + b[o]+ b[p] + b[r]+ b[s] + b[t]+ b[q] + b[u]+ b[v] + b[y]+ b[x])
